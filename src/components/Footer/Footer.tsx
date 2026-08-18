6691b7fd66d70c8a82ef974832bb8a5325174324
surprise2024-cpu <surprise2024@gmail.com>
2026-08-18 13:43:49 +0200

    Refactor Footer and Navbar styles: update layout and add copyright icon

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,28 @@
 import React from 'react'
 
+import styles from './Footer.module.css'
+
+import copyright from '../../assets/copyright-symbol.png'
+
 export const Footer = () => {
   return (
     <footer>
-        Footer
+
+        <div className={styles['footer-text-cont']}>
+
+            <div className={styles['footer-text-icon']}>
+
+                <img className={styles['copyright-icon']} src={copyright} alt='copyright icon' />
+
+            </div>
+            <div className={styles['footer-text']}>
+                
+                <span>2026 Suprise Nkosi. All rights reserved.</span>
+                
+            </div>
+            
+        </div>
+        
     </footer>
   )
 }
