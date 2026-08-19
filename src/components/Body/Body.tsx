7ca327b67df16069c9d94e82fe35fdab4858351a
surprise2024-cpu <surprise2024@gmail.com>
2026-08-19 08:36:04 +0200

    Refactor Body component: update structure, add empty state image, and enhance styling

--- src/components/Body/Body.tsx
+++ src/components/Body/Body.tsx
@@ -1,11 +1,37 @@
 import React from 'react'
 
 import styles from './Body.module.css'
+import emptyState from '../../assets/shopping.png'
 
-export const Body = () => {
+export const Body: React.FC = () => {
   return (
     <div className={styles['body-cont']}>
-        <span>Body</span>
+
+      <div className={styles['holder']}>
+
+        <div className={styles['body-text']}>
+
+          <span>List is Empty</span>
+
+        </div>
+        <div className={styles['body-image']}>
+
+          <img src={emptyState} alt='empty state' />
+
+        </div>
+        <div className={styles['body-text']}>
+
+          <span>Add your First item today</span>
+
+        </div>
+        <div className={styles['body-btn']}>
+
+          <button className={styles['add-btn']}>
+            Add Item
+          </button>
+
+        </div>
+      </div>
     </div>
   )
 }
