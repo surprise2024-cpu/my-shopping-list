0d08230e557e68abf8e651efbb38088c8b0e0a32
surprise2024-cpu <surprise2024@gmail.com>
2026-08-21 11:28:02 +0200

    Add Profile and ProfilePage components: create Profile component and integrate it into ProfilePage

---
+++ src/pages/ProfilePage.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Profile } from '../components/Profile/Profile'
+
+export const ProfilePage: React.FC = () => {
+  return (
+    <div>
+        <Profile />
+    </div>
+  )
+}
