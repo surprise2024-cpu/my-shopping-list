ac5636799a8373884d99f7bbf30759f9699d8188
surprise2024-cpu <surprise2024@gmail.com>
2026-08-21 09:31:04 +0200

    Add react-router dependency to package.json and package-lock.json for routing support

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+{/*import { useState } from 'react'*/}
 import './App.css'
 import { Navbar } from './components/Navbar/Navbar'
 import { Footer } from './components/Footer/Footer'
-import { Body } from './components/Body/Body'
+{/*import { Body } from './components/Body/Body'*/}
 import { SignIn } from './components/SignIn/SignIn'
-import { SignUp } from './components/SignUp/SignUp'
 
 function App() {
-  const [count, setCount] = useState(0)
+  {/*const [count, setCount] = useState(0)*/}
 
   return (
     <div id='app-cont'>
