80a16c41974cd9363bb768fa0da3a4c675250672
surprise2024-cpu <surprise2024@gmail.com>
2026-08-24 05:09:19 +0200

    feat: add auth and api slices with initial state and hooks

---
+++ src/store/store.tsx
@@ -0,0 +1,17 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+
+
+export const store = configureStore({
+    reducer: {
+        auth: authReducer,
+        [apiSlice.reducerPath]: apiSlice.reducer,
+    },
+    middleware: (getDefaultMiddleware) => 
+        getDefaultMiddleware().concat(apiSlice.middleware),
+})
+
+setupListeners(store.dispatch)
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
