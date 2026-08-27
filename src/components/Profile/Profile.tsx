ea653ef6656318d45d647bb77ffdeb0802d71f7d
surprise2024-cpu <surprise2024@gmail.com>
2026-08-27 09:43:58 +0200

    feat: enhance Profile component with action buttons for profile, settings, and logout

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -3,12 +3,53 @@ import React from 'react'
 import styles from './Profile.module.css'
 
 import standIn from '../../assets/profile-picture.png'
+import person from '../../assets/person.png'
+import greater from '../../assets/greater-than-symbol.png'
+import logout from '../../assets/log-out.png'
+import settings from '../../assets/setting.png'
 
 export const Profile: React.FC = () => {
   return (
     <div className={styles['profile-cont']}>
-      <div>
-        <img src={standIn} alt='placeholder' />
+
+      <div className={styles['placeholder']}>
+        
+        <img src={standIn} alt='placeholder' width={80} height={80}/>
+      </div>
+      <div className={styles['profile-info']}>
+        <button className={styles['actions']}>
+          <div>
+            <img src={person} alt='placeholder' width={30} height={30} />
+         </div>
+         <div>
+            My Profile
+         </div>
+         <div>
+            <img src={greater} alt='placeholder' width={10} height={10} />
+         </div>
+        </button>
+         <button>
+          <div>
+            <img src={settings} alt='placeholder' width={30} height={30} />
+         </div>
+         <div>
+            Settings
+         </div>
+         <div>
+            <img src={greater} alt='placeholder' width={10} height={10} />
+         </div>
+        </button>
+        <button>
+          <div>
+            <img src={logout} alt='placeholder' width={30} height={30} />
+         </div>
+         <div>
+            Log Out
+         </div>
+         <div>
+            <img src={greater} alt='placeholder' width={10} height={10} />
+         </div>
+        </button>
       </div>
     </div>
   )
