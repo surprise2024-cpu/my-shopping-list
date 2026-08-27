0d6fc5159753fed328755c4b0e123e6b5fc9847c
surprise2024-cpu <surprise2024@gmail.com>
2026-08-27 09:03:31 +0200

    fix: update API endpoint in ProfileForm component to correct server address

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -2,10 +2,14 @@ import React from 'react'
 
 import styles from './Profile.module.css'
 
+import standIn from '../../assets/profile-picture.png'
+
 export const Profile: React.FC = () => {
   return (
-    <div className={styles['form-cont']}>
-      
+    <div className={styles['profile-cont']}>
+      <div>
+        <img src={standIn} alt='placeholder' />
+      </div>
     </div>
   )
 }
