fedcbc617226a22f50439a48f8c030e1f9dcb84c
thegrindneverstops21 <ndlovusamjunior21@gmail.com>
2026-09-05 14:08:32 +0200

    feat: restructure login and registration pages with new components and styles for improved organization

---
+++ src/pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SignIn } from '../../components/Login/Login'
+
+import styles from './LoginPage.module.css'
+
+export const SignInPage = () => {
+  return (
+    <div className={styles['page-cont']}>
+        <SignIn />
+    </div>
+  )
+}
---
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SignUp } from '../../components/Register/Register'
+
+import styles from './RegisterPage.module.css'
+
+export const SignUpPage = () => {
+  return (
+    <div className={styles['page-cont']}>
+        <SignUp />
+    </div>
+  )
+}
